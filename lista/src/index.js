36f1e82c7df8a086bb7b916fbeadef8ad26be0ae
dancorrea86 <danielmacielcorrea@gmail.com>
2020-05-11 13:25:46 -0300

    commit

--- lista/src/index.js
+++ lista/src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = 
+      [ 
+        {"id": 1, "tarefas": "Tarefa 1"},
+        {"id": 2, "tarefas": "Tarefa 1"},
+        {"id": 3, "tarefas": "Tarefa 1"},
+        {"id": 4, "tarefas": "Tarefa 1"}
+      ]
+  }
+
+  renderLista() {
+    const valores = this.state((itens) => 
+      <ul>
+        <li id={valores.id}>{valores.tarefas}</li>
+      </ul>)
+  }
   render() {
     return (
-      <button>Button</button>
+      <div>
+        <button>Button</button>
+        {this.renderLista}
+      </div>
+      
     )
   }
 }
